eb5de900803dfc9b49c74ac6c0fa187f3e7a2363
Koda <koda@horizons.gg>
2024-12-25 16:51:36 +1000

    Fixed Send Message Perm

--- app/tickets/index.ts
+++ app/tickets/index.ts
@@ -70,7 +70,7 @@ export class TicketController {
 
         channel.send(`>>> ### ${this.fetchService(service)?.name}  -  ${data?.priority === 'low' ? 'Low Priority 🔷' : 'High Priority 🔶'}\n${data?.priority === 'low' ? '@here' : '@everyone'}${serviceData.role !== null ? ` <@&${serviceData.role}>` : ''}`)
 
-        channel.permissionOverwrites.create(ownerId, {
+        channel.permissionOverwrites.edit(ownerId, {
             ViewChannel: true,
             SendMessages: true,
         })
